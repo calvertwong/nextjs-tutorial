6274041560c13a3db44b61729fcf4e3f6f9cae98
Calvert <wongcalvert@gmail.com>
2021-07-19 02:45:14 -0700

    added css

--- pages/posts/first-post.tsx
+++ pages/posts/first-post.tsx
@@ -1,3 +1,26 @@
+import Image from 'next/image'
+import Head from 'next/head'
+import Link from 'next/link'
+import Layout from '../../components/layout'
+
 export default function FirstPost() {
-  return <h1>First Post</h1>
+  return (
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <Image
+        src="/images/profile.jpg"
+        height={ 200 }
+        width={ 200 }
+        alt="Alice"
+      />
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h2>
+    </Layout>
+  )
 }
